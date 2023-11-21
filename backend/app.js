355d67a330b599f919416935f32b744dcf86a46b
J0E-E <joeyiglesias83@gmail.com>
2023-11-21 17:09:19 -0500

    Cleaning up and commenting on code

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongodb = require("mongodb");
 const app = express();
 let db;
 
-// CORS middleware
+// CORS middleware for local work.
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // Allows access from any origin
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -13,9 +13,11 @@ app.use(function(req, res, next) {
     next();
 });
 
+/* For this to work, just download and install mongoDB. Once the service is running, this should connect to it with
+The default settings. */
 app.listen(8080, ()=> {
-    const mongodbUri = "mongodb://localhost:27017";
-    const client = new mongodb.MongoClient(mongodbUri);
+    const mongodbURI = "mongodb://localhost:27017";
+    const client = new mongodb.MongoClient(mongodbURI);
     client.connect().then((server) => {
         db = server.db('joeys_app')
     });
@@ -23,21 +25,25 @@ app.listen(8080, ()=> {
 });
 
 app.post("/addTask", async (req, res) => {
+    // POST method for adding tasks to the DB.
     const queryParams = req.query;
     const task = queryParams.task;
-    const uid = uuidv4()
+    // Create a UUID for saving the task to the DB.
+    const uid = uuidv4();
     await db.collection('tasks').insertOne({task, completed: false, uid: uid});
     res.status(200).json({ uid: uid})
     console.log(`Task added with uid: ${uid}`)
 })
 
 app.get("/getTasks", async (req, res) => {
+    // GET method for retrieving Tasks list.
     let data = await db.collection("tasks").find().toArray();
-    res.json(data)
+    res.status(200).json(data)
     console.log("Tasks sent.")
 })
 
 app.put("/toggleCompleted", async (req, res) => {
+    // PUT method for toggling completed status of a Task.
     const queryParams = req.query;
     const taskUID = queryParams.uid;
     let taskObject = await db.collection("tasks").findOne({uid: taskUID});
@@ -53,6 +59,7 @@ app.put("/toggleCompleted", async (req, res) => {
 });
 
 app.delete("/clearCompleted", async (req, res) => {
+    // DELETE method for removing completed tasks from the DB.
     await db.collection("tasks").deleteMany({completed: true})
     console.log("DELETED COMPLETED")
     res.sendStatus(200)
