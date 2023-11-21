107cdea5169ece04f8ff83bc665a7be131c8c840
J0E-E <joeyiglesias83@gmail.com>
2023-11-21 16:58:02 -0500

    add lint file

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.listen(8080, ()=> {
     client.connect().then((server) => {
         db = server.db('joeys_app')
     });
-      console.log("My app started.")
+    console.log("My app started.")
 });
 
 
